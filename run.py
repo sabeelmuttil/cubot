6d5e80d72b0e661d109d09face43f870009b0407
sabeel muttil <sabeelmuttil@gmail.com>
2021-08-08 20:05:06 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+
+import os
+import sys
+
+
+os.system("gnome-terminal -- python bot/main.py")  # opens bot terminal
+
+
+os.system("gnome-terminal -- python crawler/main.py")  # opens crawler terminal
