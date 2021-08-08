900afef1b9b6eb579f5d0e520d07416839754170
sabeel muttil <sabeelmuttil@gmail.com>
2021-08-08 20:01:33 +0530

    Update status.py

--- bot/status.py
+++ bot/status.py
@@ -1,11 +1,13 @@
 import telepot
 from pprint import pprint
-cubot=telepot.Bot('351057354:AAFk5gALlI2AqCqcCh4EAwR35BzSs1Kq8bA')
-updates=cubot.getUpdates()
-# pprint(updates)
-# from telepot.loop import MessageLoop
-# def handle(msg):
-#      pprint(msg)
-# MessageLoop(cubot, handle).run_as_thread()
+cubot = telepot.Bot('351057354:AAFk5gALlI2AqCqcCh4EAwR35BzSs1Kq8bA')
+updates = cubot.getUpdates()
+print(updates)
+from telepot.loop import MessageLoop
 
-# store updates to database
+
+def handle(msg):
+    print(msg)
+
+
+MessageLoop(cubot, handle).run_as_thread()
